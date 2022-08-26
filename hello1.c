9bcde2c1df4af5edc21826dacd76718ed876f398
Matt Schlatter <mschlatter2@huskers.unl.edu>
2022-08-26 21:05:40 +0000

    changed name

---
+++ hello1.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Matt Schlatter
+ *
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Matt, Computer Engineering!\n");
+  printf("Matt has no partner :(\n");
+
+  return 0;
+}
